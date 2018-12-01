2a0abaea246ee5e5589ec534e7bdbaacc6563ea4
chenqiong <741718030@qq.com>
2018-12-01 21:37:06 +0800

    add

--- src/view/shenpi.vue
+++ src/view/shenpi.vue
@@ -5,13 +5,38 @@
     </x-header>
     <div>
       <tab bar-active-color="#0084ff" active-color="#333">
-        <tab-item selected>待审批订单</tab-item>
-        <tab-item>审批需求</tab-item>
+        <tab-item selected @on-item-click="queryList">待审批订单</tab-item>
+        <tab-item @on-item-click="queryHasList">已审批订单</tab-item>
       </tab>
-      <el-collapse accordion  v-model="activeName" @change="handleChange" >
-        <el-collapse-item   v-for="(item,index) in list" :key="index" :name="index">
+      <el-collapse accordion v-show="showTabOne">
+        <el-collapse-item   v-for="(item,index) in list" :key="index">
           <template slot="title">
-            <div  style="font-size: 15px">订单号: <span style=" display:inline-block;margin-left: 10px"> {{item.sqid}}</span></div>
+            <div   style="font-size: 15px;padding-left: 15px">订单号: <span style=" display:inline-block;margin-left: 10px"> {{item.sqid}}</span></div>
+          </template>
+          <group class="title_group">
+            <cell title="申请人" :value="item.sqr"></cell>
+            <cell title="申请人电话" :value="item.sqrdh"></cell>
+            <cell title="计费方式" :value="item.jffs"></cell>
+            <cell title="用车时间" :value =" fmtSJ(item.scsj_beg,item.scsj_end)"></cell>
+            <cell title="车辆用途" :value="item.clyt"></cell>
+            <cell title="车辆型号" :value="fmtXH(item)"></cell>
+            <cell title="申请台数" :value="item.sqsl"></cell>
+            <cell title="接车地点" :value="item.jcdd"></cell>
+            <cell title="目的地" :value= "item.mdd"></cell>
+            <cell title="是否需要司机" :value ="item.xysj"></cell>
+          </group>
+          <div class="btn_group">
+            <x-button class="pass" @click.native="pass(item)">通过</x-button>
+            <x-button class="failed"  @click.native="failed(item)"> 不通过</x-button>
+          </div>
+        </el-collapse-item>
+      </el-collapse>
+
+      <el-collapse accordion v-show="!showTabOne">
+        <el-collapse-item   v-for="(item,index) in haslist" :key="index" :name="index">
+          <template slot="title">
+            <div  style="font-size: 15px;padding-left: 15px">订单号: <span style=" display:inline-block;margin-left: 10px"> {{item.sqid}}</span>
+              <span style=" display:inline-block;margin-left: 30px;color:#0084ff">{{item.shbz}}</span></div>
           </template>
           <group class="title_group">
             <cell title="申请人" :value="item.sqr"></cell>
@@ -27,10 +52,6 @@
           </group>
         </el-collapse-item>
       </el-collapse>
-      <div class="btn_group">
-        <x-button class="pass">通过</x-button>
-        <x-button class="failed"> 不通过</x-button>
-      </div>
     </div>
   </div>
 </template>
@@ -48,19 +69,15 @@ export default {
         rentTimes: '',
         duration: ''
       },
-      activeName: '',
-      list: []
+      list: [],
+      haslist: [],
+      showTabOne: true
     }
   },
   mounted () {
     this.queryList()
   },
   methods: {
-    handleChange () {
-      if (this.activeName || this.activeName === 0) {
-        console.log(this.list[this.activeName])
-      }
-    },
     fmtSJ (beg, end) {
       return beg.substring(0, 16) + ' - ' + end.substring(0, 16)
     },
@@ -68,12 +85,61 @@ export default {
       return item.car_pp + item.car_xh
     },
     queryList () {
+      this.showTabOne = true
       this.$vux.loading.show({text: '加载中'})
       let params = api.getParam('app006', {ysh: '0'})
       api.postData(this, params).then((data) => {
         this.$vux.loading.hide()
         if (data.code === 0) {
           this.list = data.data.rows
+        } else {
+          this.$vux.toast.text(data.msg, '')
+        }
+      }).catch((code) => {
+        this.$vux.loading.hide()
+        this.$vux.toast.text(code, '')
+      })
+    },
+    queryHasList () {
+      this.showTabOne = false
+      this.$vux.loading.show({text: '加载中'})
+      let params = api.getParam('app006', {ysh: '1'})
+      api.postData(this, params).then((data) => {
+        this.$vux.loading.hide()
+        if (data.code === 0) {
+          this.haslist = data.data.rows
+        } else {
+          this.$vux.toast.text(data.msg, '')
+        }
+      }).catch((code) => {
+        this.$vux.loading.hide()
+        this.$vux.toast.text(code, '')
+      })
+    },
+    pass (item) {
+      this.$vux.loading.show({text: '加载中'})
+      let params = api.getParam('app007', {'sqid': item.sqid, 'shbz': '1', 'shyj': ''})
+      api.postData(this, params).then((data) => {
+        this.$vux.loading.hide()
+        if (data.code === 0) {
+          this.queryList()
+          this.$vux.toast.text('操作成功', '')
+        } else {
+          this.$vux.toast.text(data.msg, '')
+        }
+      }).catch((code) => {
+        this.$vux.loading.hide()
+        this.$vux.toast.text(code, '')
+      })
+    },
+    failed (item) {
+      this.$vux.loading.show({text: '加载中'})
+      let params = api.getParam('app007', {'sqid': item.sqid, 'shbz': '2', 'shyj': ''})
+      api.postData(this, params).then((data) => {
+        this.$vux.loading.hide()
+        if (data.code === 0) {
+          this.queryList()
+          this.$vux.toast.text('操作成功', '')
         } else {
           this.$vux.toast.text(data.msg, '')
         }
